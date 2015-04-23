f4eea7f57200d6a13bdfde267119356d9ade2ab1
stm210 <stephanie.magnuson@gmail.com>
2015-04-22 19:13:04 -0500

    Test 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera [R] Programming Assignment 2
+## Script inverts a matrix utilizing cache to save time 
+## Inputs: an invertable matrix
+## Outputs: inverse of the matrix
 
-## Write a short comment describing this function
-
+## Part 1: set up the cache piece
 makeCacheMatrix <- function(x = matrix()) {
+            m <- NULL
+            set <- function(y) {
+               x <<- y
+               m <<- NULL
+            }
+            get <- function() x
+            
+            setsolve <- function(solve) m <<- solve
+            getsolve <- function() m
+            
+            list(set = set, get = get,
+                 setsolve = setsolve,
+                 getsolve = getsolve)
+        }
 
-}
 
-
-## Write a short comment describing this function
+## Part 2: cacheSolve function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ 
+        m <- x$getsolve()
+      
+        if(!is.null(m)) {
+          message("Getting cached data.")
+          return(m)
+        }
+        
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+       
+        m  
+  
 }
